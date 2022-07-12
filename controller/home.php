253d41e57d05e5d4a356f43b049d2fdbdfae5e70
CandyMan5 <trinhtan321@gmail.com>
2022-07-12 20:04:54 +0700

    changes2

--- controller/home.php
+++ controller/home.php
@@ -1,7 +1,7 @@
 <?php
 class HomeController {
     public function index(){
-        $VIEW = "./view/home.phtml";
+        $VIEW = "./view/employee_OT.phtml";
         require("./template/main_template.phtml");
     }
 }
